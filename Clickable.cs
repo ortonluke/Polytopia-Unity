d5fa3108f845b7629f339dcaf0acf1881e4db60c
ortonluke <ortonluke77@gmail.com>
2025-08-07 14:42:15 -0500

    Add Unity project folders and .gitignore

--- Clickable.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Clickable : MonoBehaviour
-{
-    public abstract void OnClick();
-}
--- Structure.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Structure : Clickable
-{
-    //To differentiate between buildings and tiles
-}
